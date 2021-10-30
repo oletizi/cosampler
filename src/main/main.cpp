299b07bbb2a191ed0040ea8275a862de70d5b076
orion <oletizi@mac.com>
2021-10-30 13:30:29 -0700

    - conan integration seems to work - mpc creation and init seems to work.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-#include "config.hpp"
+//#include "config.hpp"
+#include "Mpc.hpp"
 
 int main() {
+    mpc::Mpc mpc = mpc::Mpc();
+    mpc.init(44100, 2, 2);
+
     std::cout << "Hello, World!" << std::endl;
-    printf("Version: %d.%d", osampler_VERSION_MAJOR, osampler_VERSION_MINOR);
+  //  printf("Version: %d.%d", osampler_VERSION_MAJOR, osampler_VERSION_MINOR);
     return 0;
 }
